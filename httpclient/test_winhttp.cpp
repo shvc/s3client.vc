78226bf8258b678012957eae3a641db99e04e1bf
小貉 <32062902+GodOfOrder@users.noreply.github.com>
2019-05-01 19:55:56 +0800

    a test of winhttp to download html source code

---
+++ httpclient/test_winhttp.cpp
@@ -0,0 +1,109 @@
+// test_winhttp.cpp :Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <windows.h>
+#include <winhttp.h>
+#include <conio.h>
+#pragma comment(lib, "Winhttp.lib")
+
+
+void WinhttpUpload()
+{
+	DWORD dwSize = 0;
+    DWORD dwDownloaded = 0;
+    LPSTR pszOutBuffer;
+    BOOL  bResults = FALSE;
+    HINTERNET  hSession = NULL, 
+               hConnect = NULL,
+               hRequest = NULL;
+
+    // Use WinHttpOpen to obtain a session handle.
+	hSession = WinHttpOpen(NULL, WINHTTP_ACCESS_TYPE_NO_PROXY, NULL, NULL, 0);
+
+    // Specify an HTTP server.
+    if (hSession)
+        hConnect = WinHttpConnect( hSession, L"www.microsoft.com",INTERNET_DEFAULT_HTTPS_PORT, 0);
+
+    // Create an HTTP request handle.
+    if (hConnect)
+        hRequest = WinHttpOpenRequest( hConnect, L"GET", NULL,NULL, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, WINHTTP_FLAG_SECURE);
+
+    // Send a request.
+    if (hRequest)
+        bResults = WinHttpSendRequest( hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0);
+ 
+    // End the request.
+    if (bResults)
+        bResults = WinHttpReceiveResponse( hRequest, NULL);
+
+    // Keep checking for data until there is nothing left.
+    if (bResults)
+    {
+        do 
+        {
+            // Check for available data.
+            dwSize = 0;
+            if (!WinHttpQueryDataAvailable( hRequest, &dwSize)) 
+            {
+                printf( "Error %u in WinHttpQueryDataAvailable.\n",
+                        GetLastError());
+                break;
+            }
+            
+            // No more available data.
+            if (!dwSize)
+                break;
+
+            // Allocate space for the buffer.
+            pszOutBuffer = new char[dwSize+1];
+            if (!pszOutBuffer)
+            {
+                printf("Out of memory\n");
+                break;
+            }
+            
+            // Read the Data.
+            ZeroMemory(pszOutBuffer, dwSize+1);
+
+            if (!WinHttpReadData( hRequest, (LPVOID)pszOutBuffer, 
+                                  dwSize, &dwDownloaded))
+            {                                  
+                printf( "Error %u in WinHttpReadData.\n", GetLastError());
+            }
+            else
+            {
+                printf("%s", pszOutBuffer);
+            }
+        
+            // Free the memory allocated to the buffer.
+            delete [] pszOutBuffer;
+
+            // This condition should never be reached since WinHttpQueryDataAvailable
+            // reported that there are bits to read.
+            if (!dwDownloaded)
+                break;
+                
+        } while (dwSize > 0);
+    }
+    else
+    {
+        // Report any errors.
+        printf( "Error %d has occurred.\n", GetLastError() );
+    }
+
+    // Close any open handles.
+    if (hRequest) WinHttpCloseHandle(hRequest);
+    if (hConnect) WinHttpCloseHandle(hConnect);
+    if (hSession) WinHttpCloseHandle(hSession);
+}
+
+//make the features of HTTP upload and download come true
+int _tmain(int argc, _TCHAR* argv[])
+{
+	WinhttpUpload();
+	printf("Press any key to continue...");
+	_getch();
+	return 0;
+}
+
