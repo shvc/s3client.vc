d1a1ba35b0812723d482a0456e8748623a209bae
vager <vager@aweg.cc>
2019-05-05 00:40:28 +0800

    try file stream

--- curlclient/curlclient.cpp
+++ curlclient/curlclient.cpp
@@ -5,6 +5,8 @@
 #include <conio.h>
 #include <curl/curl.h>
 #include <windows.h>
+#include <iostream>
+#include <fstream>
 
 size_t libcurl_read_callback(void * pBuffer, size_t size, size_t nmemb, void * hFile)
 {
@@ -93,13 +95,28 @@ void UploadFile(char * strFileName, char * strFilePath)
 		printf("File upload failed! Could not open local file");
 }
 
+
+
+size_t libcurl_read_stream(void * pBuffer, size_t size, size_t nmemb, void * stream)
+{
+	std::streampos begin,end;
+	//DWORD dwNumberOfBytesRead = 88;
+	std::ifstream *is = (std::ifstream*)stream;
+	begin = is->tellg();
+    is->read((char*)pBuffer, size*nmemb);
+	//*is >> (char*)pBuffer;
+	end = is->tellg();
+	//BOOL bResult = ReadFile((HANDLE) hFile, pBuffer, size * nmemb, &dwNumberOfBytesRead, NULL);
+	return end - begin;
+	//return 88;
+}
 void UploadFileStream(char * strFileName, char * strFilePath)
 {
 	char strBuffer[1024];
 	CURL * hCurl;
 	CURLcode ccCurlResult = CURL_LAST;
 	curl_off_t cotFileSize;
-	HANDLE hFile;
+	//HANDLE hFile;
 	LARGE_INTEGER liFileSize;
 	// check parameters
 	if((strFileName == NULL || strlen(strFileName) == 0) ||
@@ -110,16 +127,10 @@ void UploadFileStream(char * strFileName, char * strFilePath)
 		sprintf_s(strBuffer, 1024, "%s%s", strFilePath, strFileName);
 	else
 		sprintf_s(strBuffer, 1024, "%s\\%s", strFilePath, strFileName);
-	// create a handle to the file
-	hFile = CreateFileA(strBuffer, // file to open
-			GENERIC_READ, // open for reading
-			FILE_SHARE_READ, // share for reading
-			NULL, // default security
-			OPEN_EXISTING, // existing file only
-			FILE_ATTRIBUTE_NORMAL, // normal file
-			NULL); // no attr. template
 
-	if(hFile != INVALID_HANDLE_VALUE)
+	std::ifstream filestream(strBuffer);
+
+	if(filestream.is_open())
 	{
 		// global libcurl initialisation
 		ccCurlResult = curl_global_init(CURL_GLOBAL_WIN32);
@@ -133,17 +144,21 @@ void UploadFileStream(char * strFileName, char * strFilePath)
 				curl_easy_setopt(hCurl, CURLOPT_VERBOSE, TRUE);
 				// enable uploading
 				curl_easy_setopt(hCurl, CURLOPT_UPLOAD, TRUE);
+				curl_easy_setopt(hCurl, CURLOPT_CUSTOMREQUEST, "PUT");
+				struct curl_slist *headers = NULL;
+				headers = curl_slist_append(headers, "Content-Type:application/octet-stream");
+				curl_easy_setopt(hCurl, CURLOPT_HTTPHEADER, headers);
 				// inform libcurl of the file's size
-				GetFileSizeEx(hFile, &liFileSize);
-				cotFileSize = liFileSize.QuadPart;
+				//GetFileSizeEx(hFile, &liFileSize);
+				cotFileSize = 3814;
 				curl_easy_setopt(hCurl, CURLOPT_INFILESIZE_LARGE,cotFileSize);
 				// enable progress report function
-				curl_easy_setopt(hCurl, CURLOPT_NOPROGRESS, FALSE);
-				curl_easy_setopt(hCurl, CURLOPT_PROGRESSFUNCTION,libcurl_progress_callback);
+				//curl_easy_setopt(hCurl, CURLOPT_NOPROGRESS, FALSE);
+				//curl_easy_setopt(hCurl, CURLOPT_PROGRESSFUNCTION,libcurl_progress_callback);
 				// use custom read function
-				curl_easy_setopt(hCurl, CURLOPT_READFUNCTION, libcurl_read_callback);
+				curl_easy_setopt(hCurl, CURLOPT_READFUNCTION, libcurl_read_stream);
 				// specify which file to upload
-				curl_easy_setopt(hCurl, CURLOPT_READDATA, hFile);
+				curl_easy_setopt(hCurl, CURLOPT_READDATA, &filestream);
 				// specify full path of uploaded file (i.e. server
 				// address plus remote path)
 				sprintf_s(strBuffer, 1024, "http://192.168.1.12:9000/open/%s", strFileName);
@@ -155,7 +170,7 @@ void UploadFileStream(char * strFileName, char * strFilePath)
 			}
 		}
 		// release file handle
-		CloseHandle(hFile);
+		//CloseHandle(hFile);
 		// global libcurl cleanup
 		curl_global_cleanup();
 		if (ccCurlResult == CURLE_OK)
@@ -170,7 +185,7 @@ void UploadFileStream(char * strFileName, char * strFilePath)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	UploadFile("test.txt", "C:\\");
+	UploadFileStream("test.txt", "C:\\");
 	printf("Press any key to continue...");
 	_getch();
 	return 0;
