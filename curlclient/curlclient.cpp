640938d744f89c74e514546aff684fb0635747f6
vager <vager@aweg.cc>
2019-05-06 21:41:42 +0800

    refine func parameter

--- curlclient/curlclient.cpp
+++ curlclient/curlclient.cpp
@@ -3,17 +3,15 @@
 
 #include "stdafx.h"
 #include <conio.h>
-#include <curl/curl.h>
 #include <windows.h>
 #include <iostream>
 #include <fstream>
-using namespace std;
+#include <curl/curl.h>
 
 size_t libcurl_read_callback(void * pBuffer, size_t size, size_t nmemb, void * hFile)
 {
 	DWORD dwNumberOfBytesRead = 0;
 	BOOL bResult = ReadFile((HANDLE) hFile, pBuffer, size * nmemb, &dwNumberOfBytesRead, NULL);
-
 	return dwNumberOfBytesRead;
 }
 
@@ -23,28 +21,21 @@ int libcurl_progress_callback (void * clientp, double dltotal, double dlnow,doub
 	return 0;
 }
 
-void UploadFile(char * strFileName, char * strFilePath)
+void UploadFile(char * strURL, char * strFileName)
 {
-	char strBuffer[1024];
 	CURL * hCurl;
+	HANDLE hFile;
 	CURLcode ccCurlResult = CURL_LAST;
 	// curl_off_t 
 	curl_off_t cotFileSize;
-	HANDLE hFile;
 	
 	LARGE_INTEGER liFileSize;
 	// check parameters
-	if((strFileName == NULL || strlen(strFileName) == 0) ||
-			(strFilePath == NULL || strlen(strFilePath) == 0))
+	if((strFileName == NULL || strlen(strFileName) == 0) || (strURL == NULL || strlen(strURL) == 0))
 		return;
-	// parse file path
-	if(strFilePath[strlen(strFilePath) - 1] == '\\')
 
-		sprintf_s(strBuffer, 1024, "%s%s", strFilePath, strFileName);
-	else
-		sprintf_s(strBuffer, 1024, "%s\\%s", strFilePath, strFileName);
 	// create a handle to the file
-	hFile = CreateFileA(strBuffer, // file to open
+	hFile = CreateFileA(strFileName, // file to open
 			GENERIC_READ, // open for reading
 			FILE_SHARE_READ, // share for reading
 			NULL, // default security
@@ -66,6 +57,7 @@ void UploadFile(char * strFileName, char * strFilePath)
 				curl_easy_setopt(hCurl, CURLOPT_VERBOSE, TRUE);
 				// enable uploading
 				curl_easy_setopt(hCurl, CURLOPT_UPLOAD, TRUE);
+				curl_easy_setopt(hCurl, CURLOPT_CUSTOMREQUEST, "PUT");
 				// inform libcurl of the file's size
 				GetFileSizeEx(hFile, &liFileSize);
 				cotFileSize = liFileSize.QuadPart;
@@ -74,15 +66,12 @@ void UploadFile(char * strFileName, char * strFilePath)
 				curl_easy_setopt(hCurl, CURLOPT_NOPROGRESS, FALSE);
 				// the process of upload file
 				curl_easy_setopt(hCurl, CURLOPT_PROGRESSFUNCTION,libcurl_progress_callback);
-				// use custom read function
-				// callback read the file
+				// use custom read function callback read the file
 				curl_easy_setopt(hCurl, CURLOPT_READFUNCTION, libcurl_read_callback);
 				// specify which file to upload
 				curl_easy_setopt(hCurl, CURLOPT_READDATA, hFile);
-				// specify full path of uploaded file (i.e. server
-				// address plus remote path)
-				sprintf_s(strBuffer, 1024, "http://127.0.0.1:9000/alexbucket/%s", strFileName);
-				curl_easy_setopt(hCurl, CURLOPT_URL, strBuffer);
+				// specify full path of uploaded file
+				curl_easy_setopt(hCurl, CURLOPT_URL, strURL);
 				// execute command
 				ccCurlResult = curl_easy_perform(hCurl);
 				// end libcurl easy session
@@ -106,39 +95,25 @@ void UploadFile(char * strFileName, char * strFilePath)
 size_t libcurl_read_stream(void * pBuffer, size_t size, size_t nmemb, void * stream)
 {
 	std::ifstream *is = (std::ifstream*)stream;
-	std::streambuf *psbuf_begin = is->rdbuf();
-	
-        is->read((char*)pBuffer, size*nmemb);
-
-	// streamsize = signed int  
-	std::streamsize Gcount = is->gcount();
-	return Gcount;
+	is->read((char*)pBuffer, size*nmemb); 
+	return is->gcount();
 }
 
-void UploadFileStream(char * strFileName, char * strFilePath)
+void UploadFileStream(char * strURL, char * strFileName)
 {
-	char strBuffer[1024];
 	CURL * hCurl;
-	CURLcode ccCurlResult = CURL_LAST;
 	curl_off_t cotFileSize;
-	//HANDLE hFile;
-	//LARGE_INTEGER liFileSize;
+	CURLcode ccCurlResult = CURL_LAST;
 	// check parameters
-	if((strFileName == NULL || strlen(strFileName) == 0) ||
-			(strFilePath == NULL || strlen(strFilePath) == 0))
+	if((strFileName == NULL || strlen(strFileName) == 0) || (strURL == NULL || strlen(strURL) == 0))
 		return;
-	// parse file path
-	if(strFilePath[strlen(strFilePath) - 1] == '\\')
-		sprintf_s(strBuffer, 1024, "%s%s", strFilePath, strFileName);
-	else
-		sprintf_s(strBuffer, 1024, "%s\\%s", strFilePath, strFileName);
 
 	// ifstream: read file stream
-	std::ifstream filestream(strBuffer, ios::binary);
+	std::ifstream filestream(strFileName, std::ios::binary);
 
-	filestream.seekg(0,ios::end);
-	streampos FileLength = filestream.tellg();
-	filestream.seekg(0,ios::beg);
+	filestream.seekg(0, std::ios::end);
+	std::streampos FileLength = filestream.tellg();
+	filestream.seekg(0, std::ios::beg);
 
 	if(filestream.is_open())
 	{
@@ -159,45 +134,46 @@ void UploadFileStream(char * strFileName, char * strFilePath)
 				headers = curl_slist_append(headers, "Content-Type:application/octet-stream");
 				curl_easy_setopt(hCurl, CURLOPT_HTTPHEADER, headers);
 				// inform libcurl of the file's size
-				// GetFileSizeEx(hFile, &liFileSize);
-				
 				cotFileSize = FileLength;
-				// size of file to send
-				curl_easy_setopt(hCurl, CURLOPT_INFILESIZE_LARGE,cotFileSize);
+				curl_easy_setopt(hCurl, CURLOPT_INFILESIZE_LARGE, cotFileSize);
 				// enable progress report function
 				curl_easy_setopt(hCurl, CURLOPT_NOPROGRESS, FALSE);
-				curl_easy_setopt(hCurl, CURLOPT_PROGRESSFUNCTION,libcurl_progress_callback);
-				// use custom read function
+				curl_easy_setopt(hCurl, CURLOPT_PROGRESSFUNCTION, libcurl_progress_callback);
+				// use custom read function callback read the file
 				curl_easy_setopt(hCurl, CURLOPT_READFUNCTION, libcurl_read_stream);
 				// specify which file to upload
 				curl_easy_setopt(hCurl, CURLOPT_READDATA, &filestream);
-				// specify full path of uploaded file (i.e. server
-				// address plus remote path)
-				sprintf_s(strBuffer, 1024, "http://127.0.0.1:9000/alexbucket/%s", strFileName);
-				curl_easy_setopt(hCurl, CURLOPT_URL, strBuffer);
+				// specify full URL of uploaded file
+				curl_easy_setopt(hCurl, CURLOPT_URL, strURL);
 				// execute command
 				ccCurlResult = curl_easy_perform(hCurl);
 				// end libcurl easy session
 				curl_easy_cleanup(hCurl);
 			}
 		}
-		// release file handle
-		//CloseHandle(hFile);
+
 		// global libcurl cleanup
 		curl_global_cleanup();
 		if (ccCurlResult == CURLE_OK)
 			printf("File uploaded successfully.\n");
 		else
 			printf("File upload failed. Curl error: %d\n", ccCurlResult);
+		filestream.close();
 	}
 	else
+	{
 		printf("File upload failed! Could not open local file");
+	}
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//UploadFile("winhttp.md", "D:\Typora_file");
-	UploadFileStream("winhttp.md", "D:\Typora_file");
+	// normal file upload
+	//UploadFile("http://192.168.1.6:9000/open/test.txt", "C:\\test.txt");
+
+	// stream file upload
+	UploadFileStream("http://192.168.1.6:9000/open/test.txt", "C:\\test.txt");
+
 	printf("Press any key to continue...");
 	_getch();
 	return 0;
